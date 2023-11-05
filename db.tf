a8c92de1a2737d31481e9a4e897f7f22405596c4
Silvester Silva <106209911+silvester-silva@users.noreply.github.com>
2023-11-05 17:10:19 -0300

    Update db.tf

--- db.tf
+++ db.tf
@@ -1,14 +1,45 @@
-resource "aws_db_instance" "default" {
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_db_subnet_group" "rds" {
+  name       = "main"
+
+  subnet_ids = [
+    "subnet-0c5c79843da5f2f23",
+    "subnet-002e99eab31d76b50"
+  ]
+
+  tags = {
+    Name = "tc-s1-32-subnet-group"
+  }
+}
+
+resource "aws_db_instance" "tc" {
   identifier           = "tc"
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "postgres"
   engine_version       = "15"
   instance_class       = "db.t3.micro"
-  name                 = "tc"
   username             = "postgres"
   password             = "postgres"
   parameter_group_name = "default.postgres15"
+  vpc_security_group_ids=["sg-0d74b8d63016e6404"] 
+  db_subnet_group_name = aws_db_subnet_group.rds.name
   skip_final_snapshot  = true
-  pc_security_group_ids=[sg-0d74b8d63016e6404]
 }
