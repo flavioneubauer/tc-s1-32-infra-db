687859da3303621504fbf10df3a2e8df95ca0a81
Silvester Silva <106209911+silvester-silva@users.noreply.github.com>
2023-11-04 22:59:29 -0300

    Update db.tf

--- db.tf
+++ db.tf
@@ -10,6 +10,13 @@ resource "aws_db_instance" "default" {
   password             = "postgres"
   parameter_group_name = "default.postgres15"
   skip_final_snapshot  = true
-  db_subnet_group_name   = tc-s1-32-vpc-vpc
-  vpc_security_group_ids = ["aXCiBL0DJUoZ2g.atlasv1.24cureWrIxwkr8T72lG0q6liCeJA6QKflCVpBEvyHysyRNPVrnX7BB7LRVdWyyUBlHsvpc-0cd75faecbced4a6a"]
+}
+
+resource "aws_db_subnet_group" "default" {
+  name       = "main"
+  subnet_ids = ["aXCiBL0DJUoZ2g.atlasv1.24cureWrIxwkr8T72lG0q6liCeJA6QKflCVpBEvyHysyRNPVrnX7BB7LRVdWyyUBlHsvpc-0cd75faecbced4a6a"]
+
+  tags = {
+    Name = "tc-s1-32-vpc-vpc"
+  }
 }
