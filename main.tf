dc259a01128e40d2cdaaca02cc427b8810a57778
Flavio Neubauer <flavio.neubauer@gmail.com>
2023-11-05 01:27:13 -0300

    fix name

--- main.tf
+++ main.tf
@@ -11,14 +11,13 @@ resource "aws_db_subnet_group" "rds" {
   }
 }
 
-resource "aws_db_instance" "default" {
+resource "aws_db_instance" "tc" {
   identifier           = "tc"
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "postgres"
   engine_version       = "15"
   instance_class       = "db.t3.micro"
-  name                 = "tc"
   username             = "postgres"
   password             = "postgres"
   parameter_group_name = "default.postgres15"
