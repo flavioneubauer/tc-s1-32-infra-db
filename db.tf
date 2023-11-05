85b5776f63247a4041132964aa06ea64704c834c
Silvester Silva <106209911+silvester-silva@users.noreply.github.com>
2023-11-05 16:17:54 -0300

    Update db.tf

--- db.tf
+++ db.tf
@@ -1,17 +1,3 @@
-resource "aws_db_instance" "default" {
-  identifier           = "tc"
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "postgres"
-  engine_version       = "15"
-  instance_class       = "db.t3.micro"
-  name                 = "tc"
-  username             = "postgres"
-  password             = "postgres"
-  parameter_group_name = "default.postgres15"
-  skip_final_snapshot  = true
-}
-
 resource "aws_db_subnet_group" "rds" {
   name       = "main"
 
@@ -25,4 +11,17 @@ resource "aws_db_subnet_group" "rds" {
   }
 }
 
-db_subnet_group_name = aws_db_subnet_group.rds.name
+resource "aws_db_instance" "default" {
+  identifier           = "tc"
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "postgres"
+  engine_version       = "15"
+  instance_class       = "db.t3.micro"
+  name                 = "tc"
+  username             = "postgres"
+  password             = "postgres"
+  parameter_group_name = "default.postgres15"
+  skip_final_snapshot  = true
+  db_subnet_group_name = aws_db_subnet_group.rds.name
+}
