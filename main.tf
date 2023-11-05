820dc5acca94bb595015f46ea9102d0dd42d520a
Flavio Neubauer <flavio.neubauer@gmail.com>
2023-11-05 01:42:31 -0300

    test providers

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_db_subnet_group" "rds" {
   name       = "main"
 
